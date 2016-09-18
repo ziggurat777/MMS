07125701f1970a0d2ca62fda1b4186a241855396
papago <kuo.sunnyfruit@gmail.com>
2016-09-18 11:07:41 +0800

    config

---
+++ MMS/Common/Configuration.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace MMS.Common
+{
+    public class Configuration : ConfigurationSection
+    {
+        public static Configuration Settings
+        {
+            get
+            {
+                return ConfigurationManager.GetSection("MMSConfiguration") as Configuration;
+            }
+        }
+
+        [ConfigurationProperty("Host", IsRequired = true)]
+        public string Host
+        {
+            get
+            {
+                return (string)this["Host"];
+            }
+        }
+
+        [ConfigurationProperty("members", IsRequired = true)]
+        public string Members
+        {
+            get
+            {
+                return (string)this["members"];
+            }
+        }
+
+        [ConfigurationProperty("create", IsRequired = true)]
+        public string Create
+        {
+            get
+            {
+                return (string)this["create"];
+            }
+        }
+
+        [ConfigurationProperty("edit", IsRequired = true)]
+        public string Edit
+        {
+            get
+            {
+                return (string)this["edit"];
+            }
+        }
+
+        [ConfigurationProperty("delete", IsRequired = true)]
+        public string Delete
+        {
+            get
+            {
+                return (string)this["delete"];
+            }
+        }
+
+        [ConfigurationProperty("search", IsRequired = true)]
+        public string Search
+        {
+            get
+            {
+                return (string)this["search"];
+            }
+        }
+    }
+}
--- MMS/Controllers/HomeController.cs
+++ MMS/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
-﻿using MMS.Models;
+﻿using MMS.Common;
+using MMS.Models;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -71,72 +71,5 @@ namespace MMS.Controllers
             return Out;
 
         }
-
-    }
-
-    public class Configuration : ConfigurationSection
-    {
-        public static Configuration Settings
-        {
-            get
-            {
-                return ConfigurationManager.GetSection("MMSConfiguration") as Configuration;
-            }
-        }
-
-        [ConfigurationProperty("Host", IsRequired = true)]
-        public string Host
-        {
-            get
-            {
-                return (string)this["Host"];
-            }
-        }
-
-        [ConfigurationProperty("members", IsRequired = true)]
-        public string Members
-        {
-            get
-            {
-                return (string)this["members"];
-            }
-        }
-
-        [ConfigurationProperty("create", IsRequired = true)]
-        public string Create
-        {
-            get
-            {
-                return (string)this["create"];
-            }
-        }
-
-        [ConfigurationProperty("edit", IsRequired = true)]
-        public string Edit
-        {
-            get
-            {
-                return (string)this["edit"];
-            }
-        }
-
-        [ConfigurationProperty("delete", IsRequired = true)]
-        public string Delete
-        {
-            get
-            {
-                return (string)this["delete"];
-            }
-        }
-
-        [ConfigurationProperty("search", IsRequired = true)]
-        public string Search
-        {
-            get
-            {
-                return (string)this["search"];
-            }
-        }
-
     }
 }
